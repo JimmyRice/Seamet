177bc6d476e2d789c1e6cd7a0b398e6c7eefe281
Jimmy0w0 <jimmy@mail.jimmy0w0.me>
2022-05-13 00:26:20 +0800

    Add article list view

--- Seamet/Cells/Article/ArticleList.swift
+++ Seamet/Cells/Article/ArticleList.swift
@@ -1,24 +1,30 @@
 //
-//  ArticleListView.swift
+//  ArticleList.swift
 //  Seamet
 //
-//  Created by Jimmy on 5/8/22.
+//  Created by Jimmy on 5/13/22.
 //
 
 import SwiftUI
 
-struct ArticleListView: View {
+struct ArticleList: View {
+    var articles: [Article] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List(articles, id: \.self) {article in
+                ArticleListCell(title: article.title, dateTime: DatetimeUtil.ISO8601ToString(dateTime: article.createdDate))
+            }
+        }
     }
 }
 
-struct ArticleListView_Previews: PreviewProvider {
+struct ArticleList_Previews: PreviewProvider {
     static let articles = [Article(id: 1, title: "1", createdDate: "2022-04-21T12:16:00", tags: ["Only for test"], content: "", author: 1, slug: "", category: 1),
         Article(id: 2, title: "2", createdDate: "2022-04-21T12:16:00", tags: ["Only for test"], content: "", author: 1, slug: "", category: 1),
         Article(id: 3, title: "3", createdDate: "2022-04-21T12:16:00", tags: ["Only for test"], content: "", author: 1, slug: "", category: 1)]
-    
+
     static var previews: some View {
-        ArticleListView()
+        ArticleList(articles: articles)
     }
 }
