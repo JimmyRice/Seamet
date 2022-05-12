57e4fd43b8cff4bedbc85e0a3a822281d341614a
Jimmy0w0 <jimmy@mail.jimmy0w0.me>
2022-05-13 00:00:47 +0800

    Completed article list cell

--- Seamet/Cells/Article/ArticleListCell.swift
+++ Seamet/Cells/Article/ArticleListCell.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct ArticleListCell: View {
+    var title: String
+    var dateTime: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text(title)
+                .fontWeight(.bold)
+            
+            Spacer()
+            
+            Text(dateTime)
+                .foregroundColor(.gray)
+        }
     }
 }
 
 struct ArticleListCell_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleListCell()
+        ArticleListCell(title: "Title", dateTime: "2022/03/10")
     }
 }
