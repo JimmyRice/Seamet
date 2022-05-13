317403ec76dcfb9d2dc5e5cecaa88db3880aebe5
Jimmy0w0 <jimmy@mail.jimmy0w0.me>
2022-05-13 23:58:43 +0800

    Add ArticleDetail page

---
+++ Seamet/Pages/ArticleDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleDetailView.swift
+//  Seamet
+//
+//  Created by Jimmy on 5/13/22.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleDetailView()
+    }
+}
