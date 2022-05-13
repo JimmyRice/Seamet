6b20789862b99afa26c8963963f2eae9f8c6ab7d
Jimmy0w0 <jimmy@mail.jimmy0w0.me>
2022-05-14 00:03:12 +0800

    Update ArticleProvider and create swift test

--- Seamet/Providers/ArticleProvider.swift
+++ Seamet/Providers/ArticleProvider.swift
@@ -16,9 +16,19 @@ struct ArticleProvider {
     
     static func getArticles() async throws -> [Article] {
         do {
-            let response: Entry<[Article]> = try await httpClient.getJson(addtionalUrl: "/items/article?sort=-id")
+            let response: Entry<[Article]> = try await httpClient.getJson(addtionalUrl: "/items/article?sort=-id&fields=id, title, createdDate")
             
             return response.data
+        } catch {
+            throw ArticleProviderError.responseToStructError
+        }
+    }
+    
+    static func getArticle(id: Int) async throws -> Article {
+        do {
+            let response: Article = try await httpClient.getJson(addtionalUrl: "/item/article/\(id)")
+            
+            return response
         } catch {
             throw ArticleProviderError.responseToStructError
         }
