9ffcaf7a4eca9adf1ceed13ae8c5aa80da6b8129
Jimmy0w0 <jimmy@mail.jimmy0w0.me>
2022-05-18 23:38:13 +0800

    Add a widget

---
+++ Seamet Widget/Seamet_Widget.swift
@@ -0,0 +1,70 @@
+//
+//  Seamet_Widget.swift
+//  Seamet Widget
+//
+//  Created by Jimmy on 5/18/22.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+}
+
+struct Seamet_WidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+            .fontWeight(.bold)
+    }
+}
+
+@main
+struct Seamet_Widget: Widget {
+    let kind: String = "Seamet_Widget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            Seamet_WidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Seamet")
+        .description("This is a widget shows recent article")
+    }
+}
+
+struct Seamet_Widget_Previews: PreviewProvider {
+    static var previews: some View {
+        Seamet_WidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
