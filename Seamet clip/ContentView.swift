1b46bad0cea97cf31b9b7aa6f3c84df3c899b837
Jimmy0w0 <jimmy@mail.jimmy0w0.me>
2022-05-26 16:32:49 +0800

    having fun with App clips :-)

---
+++ Seamet clip/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Seamet clip
+//
+//  Created by Jimmy on 5/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ArticleListView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Seamet clip/Seamet_clipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seamet_clipApp.swift
+//  Seamet clip
+//
+//  Created by Jimmy on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Seamet_clipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
